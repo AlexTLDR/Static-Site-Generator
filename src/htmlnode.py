18c574179b1462e8b03c3575ce7913db663517e6
AlexTLDR <alex.badragan@protonmail.com>
2025-01-17 19:11:14 +0100

    text node to html node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,10 +21,38 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
         if value is None:
             raise ValueError("LeafNode must have a value")
-        
+
     def to_html(self):
         if self.value is None:
             raise ValueError("LeafNode must have a value")
         if self.tag is None:
             return self.value
+        if self.tag == "img":
+            return f"<{self.tag}{self.props_to_html()}>"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        if tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if children is None:
+            raise ValueError("ParentNode must have children")
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+            
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+
+
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
-
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_no_props(self):
@@ -49,7 +50,117 @@ class TestLeafNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             LeafNode("p", None)
 
-    
+class TestParentNode(unittest.TestCase):
+    def test_parent_node_basic(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ]
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+        )
+
+    def test_nested_parent_nodes(self):
+        node = ParentNode(
+            "div",
+            [
+                ParentNode(
+                    "p",
+                    [
+                        LeafNode("b", "Bold text"),
+                        LeafNode(None, "Normal text"),
+                    ]
+                ),
+                ParentNode(
+                    "p",
+                    [
+                        LeafNode("i", "italic text"),
+                        LeafNode(None, "Normal text"),
+                    ]
+                )
+            ]
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<div><p><b>Bold text</b>Normal text</p><p><i>italic text</i>Normal text</p></div>"
+        )
+
+    def test_parent_node_with_props(self):
+        node = ParentNode(
+            "div",
+            [LeafNode(None, "Hello, world!")],
+            {"class": "greeting", "id": "hello"}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<div class="greeting" id="hello">Hello, world!</div>'
+        )
+
+    def test_missing_tag_raises_error(self):
+        with self.assertRaises(ValueError):
+            ParentNode(None, [LeafNode(None, "Test")])
+
+    def test_missing_children_raises_error(self):
+        with self.assertRaises(ValueError):
+            ParentNode("div", None)
+
+    def test_empty_children_list(self):
+        node = ParentNode("div", [])
+        self.assertEqual(node.to_html(), "<div></div>")
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_node_to_html_node_text(self):
+        node = TextNode("This is a text", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.to_html(), "This is a text")
+
+    def test_text_node_to_html_node_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.to_html(), "<b>This is bold</b>")
+
+    def test_text_node_to_html_node_italic(self):
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.to_html(), "<i>This is italic</i>")
+
+    def test_text_node_to_html_node_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.to_html(), "<code>This is code</code>")
+
+    def test_text_node_to_html_node_link(self):
+        node = TextNode("Click me", TextType.LINK, "https://www.example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(
+            html_node.to_html(),
+            '<a href="https://www.example.com">Click me</a>'
+        )
+
+    def test_text_node_to_html_node_image(self):
+        node = TextNode("Alt text", TextType.IMAGE, "https://www.example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(
+            html_node.to_html(),
+            '<img src="https://www.example.com/image.png" alt="Alt text">'
+        )
+
+    def test_text_node_to_html_node_invalid(self):
+        node = TextNode("Invalid type", "invalid")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
